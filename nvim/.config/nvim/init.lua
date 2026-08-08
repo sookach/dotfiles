4d149071b618894ef3a7292e8b1824a9952155d6
Andrew Sukach <github@sukach.org>
2026-08-08 19:06:05 -0400

    clean up neovim config, enable treesitter by default

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,8 @@
-vim.opt.clipboard = "unnamedplus"
-vim.opt.termguicolors = true
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+vim.opt.clipboard = "unnamedplus"
+vim.opt.termguicolors = true
 vim.opt.nu = true
 vim.opt.rnu = true
 vim.opt.mouse = 'a'
@@ -10,56 +11,52 @@ vim.opt.tabstop = 2
 vim.opt.expandtab = true
 vim.opt.mousescroll = "ver:1,hor:1"
 
+-- UI & Search Optimizations
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.updatetime = 250
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
-vim.keymap.set({ 'n' }, 'tl', ':tabnext<cr>')
-vim.keymap.set({ 'n' }, 'th', ':tabprevious<cr>')
-vim.keymap.set({ 'n' }, 'tj', ':tabnew<cr>')
-vim.keymap.set({ 'n' }, 'tk', ':tabclose<cr>')
 
-vim.keymap.set({ 'n', 'v' }, 'gf', vim.lsp.buf.format)
-vim.keymap.set({ 'n', 'v' }, 'grd', vim.lsp.buf.definition)
+vim.keymap.set('n', 'tl', ':tabnext<cr>')
+vim.keymap.set('n', 'th', ':tabprevious<cr>')
+vim.keymap.set('n', 'tj', ':tabnew<cr>')
+vim.keymap.set('n', 'tk', ':tabclose<cr>')
 
 
-vim.lsp.enable('clangd')
-vim.lsp.enable('cmake')
-vim.lsp.enable('lua_ls')
-
-vim.pack.add { 'https://github.com/ibhagwan/fzf-lua.git' }
-vim.pack.add { 'https://github.com/nvim-treesitter/nvim-treesitter' }
 vim.pack.add {
-  'https://github.com/saghen/blink.lib',
-  'https://github.com/saghen/blink.cmp'
+  'https://github.com/scottmckendry/cyberdream.nvim.git',
+  'https://github.com/ntbbloodbath/doom-one.nvim'
+}
+require('cyberdream').setup {
+  transparent = true,
+  cache = true,
+  styles = {
+    sidebars = "transparent",
+    floats = "transparent",
+  },
 }
 
-local cmp = require('blink.cmp')
-cmp.build():pwait()
-cmp.setup()
+vim.g.doom_one_transparent_background = true
 
-vim.pack.add { 'https://github.com/mikavilpas/yazi.nvim.git' }
-vim.pack.add { 'https://github.com/nvim-lua/plenary.nvim.git' }
-
-vim.keymap.set({ 'n', 'v' }, '<leader>y', ':Yazi<cr>')
+vim.cmd.colorscheme("cyberdream")
 
 vim.pack.add {
   'https://github.com/nvim-tree/nvim-web-devicons',
   'https://github.com/nvim-lualine/lualine.nvim'
 }
-require('lualine').setup {
-  options = {
-    theme = "auto", -- "auto" will set the theme dynamically based on the colorscheme
-  },
-}
+require('lualine').setup { options = { theme = "auto" } }
 
 vim.pack.add {
-  'https://github.com/folke/noice.nvim.git',
   'https://github.com/MunifTanjim/nui.nvim.git',
-  'https://github.com/rcarriga/nvim-notify.git'
+  'https://github.com/rcarriga/nvim-notify.git',
+  'https://github.com/folke/noice.nvim.git'
 }
-require("notify").setup({
-  background_colour = "#000000",
-})
-
-require("noice").setup({
+require("notify").setup { background_colour = "#000000" }
+require("noice").setup {
   lsp = {
     override = {
       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -67,45 +64,92 @@ require("noice").setup({
     },
   },
   presets = {
-    bottom_search = false,        -- use a classic bottom cmdline for search
-    command_palette = true,       -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false,       -- add a border to hover docs and signature help
+    bottom_search = false,
+    command_palette = true,
+    long_message_to_split = true,
+    inc_rename = false,
+    lsp_doc_border = false,
   },
+}
+
+vim.pack.add { "https://github.com/sphamba/smear-cursor.nvim" }
+require('smear_cursor').setup {}
+
+vim.pack.add { 'https://github.com/nvim-lua/plenary.nvim.git' }
+vim.pack.add { 'https://github.com/mikavilpas/yazi.nvim.git' }
+vim.keymap.set({ 'n', 'v' }, '<leader>y', ':Yazi<cr>')
+
+vim.pack.add { 'https://github.com/ibhagwan/fzf-lua.git' }
+local fzf = require('fzf-lua')
+vim.keymap.set('n', '<leader>ff', fzf.files, { desc = "Find Files" })
+vim.keymap.set('n', '<leader>fg', fzf.live_grep, { desc = "Live Grep" })
+
+vim.pack.add { 'https://github.com/nvim-treesitter/nvim-treesitter' }
+require('nvim-treesitter').install {
+  'awk',
+  'bash',
+  'c',
+  'cmake',
+  'cpp',
+  'git_config',
+  'git_rebase',
+  'gitattributes',
+  'gitcommit',
+  'gitignore',
+  'json',
+  'llvm',
+  'lua',
+  'make',
+  'mlir',
+  'python',
+  'vim',
+  'vimdoc',
+  'yaml',
+  'zsh',
+}
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    -- Start Treesitter
+    pcall(vim.treesitter.start)
+
+    -- Enable Folding (only set if parser is available)
+    if pcall(vim.treesitter.get_parser) then
+      vim.wo[0][0].foldmethod = 'expr'
+      vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      -- Keep folds open by default when opening a file (optional but recommended)
+      vim.wo.foldlevel = 99
+    end
+
+    -- Enable Experimental Tree-sitter Indentation
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
 })
 
--- 1. Load cyberdream using Neovim's native pack add
-vim.pack.add { 'https://github.com/scottmckendry/cyberdream.nvim.git' }
+vim.pack.add {
+  'https://github.com/saghen/blink.lib',
+  'https://github.com/saghen/blink.cmp'
+}
+require('blink.cmp').setup {
+  keymap = { preset = 'default' },
+  appearance = { use_nvim_cmp_as_default = true },
+  sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+}
 
--- 2. Configure Cyberdream options
-require("cyberdream").setup({
-  -- Enable transparent background to let Ghostty's glass show through
-  transparent = true,
+vim.keymap.set({ 'n', 'v' }, 'gf', vim.lsp.buf.format)
+vim.keymap.set({ 'n', 'v' }, 'grd', vim.lsp.buf.definition)
 
-  -- High contrast Vivaldi/Cyberpunk vibes
-  cache = true, -- Improves startup speed
-  styles = {
-    sidebars = "transparent",
-    floats = "transparent",
-  },
-})
-
--- 3. Set the colorscheme
-vim.cmd("colorscheme cyberdream")
+vim.lsp.enable('clangd')
+vim.lsp.enable('cmake')
+vim.lsp.enable('lua_ls')
 
 vim.api.nvim_create_user_command("PackClean", function()
   local inactive = {}
-
-  -- Gather all plugins tracked by the engine
   for _, p in ipairs(vim.pack.get()) do
-    -- If it's on disk but not currently called by vim.pack.add()
     if not p.active then
       table.insert(inactive, p.spec.name)
     end
   end
 
-  -- Delete them if any are found
   if #inactive > 0 then
     vim.pack.del(inactive)
     print("Cleaned up " .. #inactive .. " inactive plugin(s).")
@@ -113,11 +157,3 @@ vim.api.nvim_create_user_command("PackClean", function()
     print("Your native plugins are already clean!")
   end
 end, {})
-
-vim.pack.add { "https://github.com/sphamba/smear-cursor.nvim" }
-require('smear_cursor').setup {}
-
--- vim.pack.add { 'https://github.com/rktjmp/lush.nvim' }
--- vim.pack.add { 'https://github.com/uloco/bluloco.nvim' }
--- require('bluloco').setup { transparent = true }
--- vim.cmd.colorscheme('bluloco')
