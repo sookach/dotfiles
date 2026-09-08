70e20cf8f63400003ba7c5f3258a4f6cdcdf1e8c
Andrew Sukach <github@sukach.org>
2026-09-08 10:14:42 -0400

    sketchybar: use builtin for current-space

--- sketchybar/.config/sketchybar/plugins/current-space.sh
+++ sketchybar/.config/sketchybar/plugins/current-space.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-space=$(
-  /opt/homebrew/bin/yabai -m query --spaces --space 2>/dev/null \
-    | /usr/bin/jq -r '.index'
-)
+space=`jq '.["display-1"]' <<< $INFO`
 
 if [[ "$space" =~ ^[0-9]+$ ]]; then
     sketchybar --set "$NAME" label="$space"
