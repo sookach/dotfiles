6489faf45c1d1441f4fa99f76e6b8d68985b1587
Andrew Sukach <github@sukach.org>
2026-09-07 22:14:17 -0400

    sketchybar: batter plugin

--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -1,33 +1,39 @@
 #!/usr/bin/env bash
 
-PERCENTAGE=$(pmset -g batt | grep -o '[0-9]\+%' | cut -d% -f1)
+BATTERY_INFO=$(pmset -g batt)
+PERCENTAGE=$(printf '%s\n' "$BATTERY_INFO" | grep -o '[0-9]\+%' | cut -d% -f1)
 
 if [[ -z "$PERCENTAGE" ]]; then
-    exit 0
+  exit 0
 fi
 
 case ${PERCENTAGE} in
 9[0-9] | 100)
-    ICON=""
-    ICON_COLOR="0xff40a02b"
-    ;;
+  ICON=""
+  # ICON_COLOR="0xff40a02b"
+  ;;
 [6-8][0-9])
-    ICON=""
-    ICON_COLOR="0xff179299"
-    ;;
+  ICON=""
+  # ICON_COLOR="0xff179299"
+  ;;
 [3-5][0-9])
-    ICON=""
-    ICON_COLOR="0xfffe640b"
-    ;;
+  ICON=""
+  # ICON_COLOR="0xfffe640b"
+  ;;
 [1-2][0-9])
-    ICON=""
-    ICON_COLOR="0xffdf8e1d"
-    ;;
+  ICON=""
+  # ICON_COLOR="0xffdf8e1d"
+  ;;
 *)
-    ICON=""
-    ICON_COLOR="0xffd20f39"
-    ;;
+  ICON=""
+  # ICON_COLOR="0xffd20f39"
+  ;;
 esac
+
+ICON_COLOR='0xffffffff'
+if printf '%s\n' "$BATTERY_INFO" | grep -q 'AC Power'; then
+  ICON_COLOR='0xffceff00'
+fi
 
 sketchybar --set "$NAME" \
     icon="$ICON" \
