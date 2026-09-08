a5ce838bee85932a17e361d24361225be6a99d81
Andrew Sukach <github@sukach.org>
2026-09-07 23:45:49 -0400

    sketchybar: match battery icon sizes

--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -3,9 +3,11 @@
 BATTERY_INFO=$(pmset -g batt)
 PERCENTAGE=$(printf '%s\n' "$BATTERY_INFO" | grep -o '[0-9]\+%' | cut -d% -f1)
 
+FONT="NotoSansM Nerd Font Mono:Bold:12.0"
 CHARGE=0
 if printf '%s\n' "$BATTERY_INFO" | grep -q 'AC Power'; then
-   CHARGE=1 
+  CHARGE=1 
+  FONT="NotoSansM Nerd Font Mono:Bold:18.0"
 fi
 
 if [[ -z "$PERCENTAGE" ]]; then
@@ -52,4 +54,5 @@ esac
 
 sketchybar --set "$NAME" \
     icon="$ICON" \
+    icon.font="$FONT" \
     label="${PERCENTAGE}%"
