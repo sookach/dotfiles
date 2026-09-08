6dbdee7db16faf049bdbfe65f4f0ec0f4a912e67
Andrew Sukach <github@sukach.org>
2026-09-07 23:03:07 -0400

    sketchybar: battery icon

--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -3,30 +3,50 @@
 BATTERY_INFO=$(pmset -g batt)
 PERCENTAGE=$(printf '%s\n' "$BATTERY_INFO" | grep -o '[0-9]\+%' | cut -d% -f1)
 
+CHARGE=0
+if printf '%s\n' "$BATTERY_INFO" | grep -q 'AC Power'; then
+   CHARGE=1 
+fi
+
 if [[ -z "$PERCENTAGE" ]]; then
   exit 0
 fi
 
 case ${PERCENTAGE} in
 9[0-9] | 100)
-  ICON=""
-  # ICON_COLOR="0xff40a02b"
+  if (( CHARGE == 1 )); then
+    ICON='󰂅'
+  else
+    ICON='󰁹'
+  fi
   ;;
 [6-8][0-9])
-  ICON=""
-  # ICON_COLOR="0xff179299"
+  if (( CHARGE == 1 )); then
+    ICON='󰂊'
+  else
+    ICON='󰂁'
+  fi
   ;;
 [3-5][0-9])
-  ICON=""
-  # ICON_COLOR="0xfffe640b"
+  if (( CHARGE == 1 )); then
+    ICON='󰢝'
+  else
+    ICON='󰁾'
+  fi
   ;;
 [1-2][0-9])
-  ICON=""
-  # ICON_COLOR="0xffdf8e1d"
+  if (( CHARGE == 1 )); then
+    ICON='󰂆'
+  else
+    ICON='󰁻'
+  fi
   ;;
 *)
-  ICON=""
-  # ICON_COLOR="0xffd20f39"
+  if (( CHARGE == 1 )); then
+    ICON='󰢟'
+  else
+    ICON='󰂎'
+  fi
   ;;
 esac
 
